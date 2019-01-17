3aa816f24e9535eabb0ca63e61c77a06c1ce76b6
nafee <nafee@ualberta.ca>
2019-01-16 21:07:39 -0700

    4th commit

--- app/src/main/java/com/example/nafee/nafee_cardiobook/MainActivity.java
+++ app/src/main/java/com/example/nafee/nafee_cardiobook/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.nafee.nafee_cardiobook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
